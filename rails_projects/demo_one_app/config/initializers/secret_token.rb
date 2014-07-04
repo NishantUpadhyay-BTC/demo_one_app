b45e2c126ac978dbb99c85c128e4e236e79a60ae
NishantUpadhyay-BTC <nishantupadhyay26@gmail.com>
2014-07-04 17:42:21 +0530

    Finish commit

---
+++ rails_projects/demo_one_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoOneApp::Application.config.secret_key_base = 'ca12c5c88be9902e580e0c1b4fef6065755100e8cb8d7edb961fa8e232519477d4d77fff573630cbfde5d2b54fcb9ddf2c4e7ba4bcd908d31479e9b1ad6a091c'
---
+++ rails_projects/demo_one_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DemoOneApp::Application.config.session_store :cookie_store, key: '_demo_one_app_session'
